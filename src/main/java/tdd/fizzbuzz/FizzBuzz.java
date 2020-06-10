52c7073a29644945b5e94e0b03f8e8758c53898a
onakrobe <robert.onak@orange.com>
2020-06-10 17:25:25 +0200

    Rozwiązanie zadania nr 1

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,11 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+	String play(int i) {
+		if (i == 0) return "0";
+		if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
+		if (i % 3 == 0) return "Fizz";
+		if (i % 5 == 0) return "Buzz";
+		return String.valueOf(i);
+	}
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,105 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
-    @Test
-    public void startHere() {
-        assertThat(4).isEqualTo(5);
-    }
+	private FizzBuzz fizzBuzz;
+
+	@BeforeEach
+	void setUp() {
+		fizzBuzz = new FizzBuzz();
+	}
+
+	@Test
+	void forValue0_shouldReturn0() {
+
+		String result = fizzBuzz.play(0);
+
+		assertThat(result).isEqualTo("0");
+	}
+
+	@Test
+	void forValue1_shouldReturn1() {
+
+		String result = fizzBuzz.play(1);
+
+		assertThat(result).isEqualTo("1");
+	}
+
+	@Test
+	void forValue3_shouldReturnFizz() {
+
+		String result = fizzBuzz.play(3);
+
+		assertThat(result).isEqualTo("Fizz");
+	}
+
+	@Test
+	void forNumbersDivisibleBy3_shouldReturnFizz() {
+
+		String result1 = fizzBuzz.play(3);
+		String result2 = fizzBuzz.play(6);
+		String result3 = fizzBuzz.play(99);
+
+		assertThat(result1).isEqualTo("Fizz");
+		assertThat(result2).isEqualTo("Fizz");
+		assertThat(result3).isEqualTo("Fizz");
+	}
+
+	@Test
+	void forValue5_shouldReturnBuzz() {
+
+		String result = fizzBuzz.play(5);
+
+		assertThat(result).isEqualTo("Buzz");
+	}
+
+	@Test
+	void forNumbersDivisibleBy5_shouldReturnBuzz() {
+
+		String result1 = fizzBuzz.play(10);
+		String result2 = fizzBuzz.play(55);
+		String result3 = fizzBuzz.play(950);
+
+		assertThat(result1).isEqualTo("Buzz");
+		assertThat(result2).isEqualTo("Buzz");
+		assertThat(result3).isEqualTo("Buzz");
+	}
+
+	@Test
+	void forNumbersNotDivisibleBy3And5_shouldReturnValue() {
+
+		String result1 = fizzBuzz.play(4);
+		String result2 = fizzBuzz.play(11);
+		String result3 = fizzBuzz.play(701);
+
+		assertThat(result1).isEqualTo("4");
+		assertThat(result2).isEqualTo("11");
+		assertThat(result3).isEqualTo("701");
+	}
+
+	@Test
+	void forValue15_shouldReturnFizzBuzz() {
+
+		String result = fizzBuzz.play(15);
+
+		assertThat(result).isEqualTo("FizzBuzz");
+	}
+
+	@Test
+	void forNumbersDivisibleBy3And5_shouldReturnFizzBuzz() {
+
+		String result1 = fizzBuzz.play(45);
+		String result2 = fizzBuzz.play(150);
+		String result3 = fizzBuzz.play(105);
+
+		assertThat(result1).isEqualTo("FizzBuzz");
+		assertThat(result2).isEqualTo("FizzBuzz");
+		assertThat(result3).isEqualTo("FizzBuzz");
+	}
+
 }
